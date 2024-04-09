5a92ec67df8bc4c22dc382627a6849402993a05d
Contubernio <155535799+Contubernio@users.noreply.github.com>
2024-04-09 18:07:16 +0200

    Update ft_aux_pf.c 1807

--- 2try/ft_aux_pf.c
+++ 2try/ft_aux_pf.c
@@ -10,89 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-include "ft_printf.h"
+#include "ft_printf.h"
 
-size_t	print_spf(const char c, va_list ap)
+int	ft_putchar(const char c)
 {
-	size_t	count;
+	int	result;
 
-	count = 0;
-	if (c == 'c')
-		count += ft_putchar(va_arg(ap, int));
-	else if (c == '%')
-		count += ft_putchar('%');
-	else if (c == 's')
-		count += ft_putstr(va_arg(ap, char *));
-	else if (c == 'd' || c == 'i')
-		count += ft_putnbr_base(va_arg(ap, int), DECIMAL, 10, c);
-	else if (c == 'u')
-		count += ft_putnbr_base(va_arg(ap, unsigned), DECIMAL, 10, c);
-	else if (c == 'x')
-		count += ft_putnbr_base(va_arg(ap, unsigned), LOWER_HEX, 16, c);
-	else if (c == 'X')
-		count += ft_putnbr_base(va_arg(ap, unsigned), UPPER_HEX, 16, c);
-	else if (c == 'p')
-		count += ft_putnbr_base(va_arg(ap, long), LOWER_HEX, 16, c);
-	return (count);
+	result = write(1, &c, 1);
+	return (result);
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_putstr(const char *str)
 {
-	va_list	ap;
-	size_t	count;
-	size_t	i;
+	int	i;
 
-	count = 0;
+	if (!str)
+		return (ft_putstr("(null)"));
 	i = 0;
-	va_start(ap, str);
 	while (str[i])
 	{
-		if (str[i] == '%' && ft_strchr(SPF, str[i + 1]))
+		if (ft_putchar(str[i]) == -1)
 		{
-			count += print_spf(str[i + 1], ap);
-			i++;
+			return (-1);
 		}
-		else
-		{
-			count += ft_putchar(str[i]);
-		}
-		i++;
+		++i;
 	}
-	va_end(ap);
-	return (count);
+	return (i);
 }
-/*
-int	main(void)
+
+char	*ft_strchr(const char *s, int c)
 {
-	#include "ft_printf.h"
-	#include <stdio.h>
-    int num = -42;
-    unsigned int u_num = 4294967295;
-    char *str = "Ejemplo de cadena";
-    char c = 'A';
-    void *ptr = (void *)str;
+	size_t	i;
 
-    ft_printf("Prueba de caracter: %c\n", c);
-    printf("Prueba original (printf): %c\n\n", c);
-
-    ft_printf("Prueba de cadena: %s\n", str);
-    printf("Prueba original (printf): %s\n\n", str);
-
-    ft_printf("Prueba de numero decimal (int): %d\n", num);
-    printf("Prueba original (printf): %d\n\n", num);
-
-    ft_printf("Prueba de numero decimal sin signo (unsigned): %u\n", u_num);
-    printf("Prueba original (printf): %u\n\n", u_num);
-
-    ft_printf("Prueba de numero hexadecimal (minusculas): %x\n", u_num);
-    printf("Prueba original (printf): %x\n\n", u_num);
-
-    ft_printf("Prueba de numero hexadecimal (mayusculas): %X\n", u_num);
-    printf("Prueba original (printf): %X\n\n", u_num);
-
-    ft_printf("Prueba de puntero: %p\n", ptr);
-    printf("Prueba original (printf): %p\n", ptr);
-
-    return 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		++i;
+	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
+	return (0);
 }
-*/
